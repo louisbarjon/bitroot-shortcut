22503d6a9623ca0527de663441994905e0cccccb
Louis Barjon <louis.barjon@dataiku.com>
2025-06-13 16:05:59 +0200

    return string

--- bitroot_shortcut/main.py
+++ bitroot_shortcut/main.py
@@ -1,3 +1,4 @@
 # bitroot_shortcut/main.py
 def external_repo_working():
-    print("The external repository import is working correctly!")
+    return "The external repository import is working correctly!"
+
